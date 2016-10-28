47569100e6506fac6fe036a00da52ca5c8963c5b
EQwon <bluetwintail@naver.com>
2016-10-29 04:04:53 +0900

    Orthographic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,30 +4,36 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 	public float PlayerSpeed;
+	public float StairHeight;
+	public float StairWidth;
 
 	public bool IsOppositeSide;
-
+	public bool IsGetStair;
 
 	void Start()
 	{
 		IsOppositeSide = false;
+		IsGetStair = false;
+		StairHeight = 0;
+		StairWidth = 0;
 	}
 
 	void Update()
 	{
 		MovingKeyInput ();
 		MoveToOppositeSide ();
+		MoveByStair ();
 	}
 
 	void MovingKeyInput ()
 	{
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
-			GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x + 0.05f, GetComponent<Transform>().position.y, 0);
+			GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x + 0.1f, GetComponent<Transform>().position.y, 0);
 		}
 		else if (Input.GetKey(KeyCode.LeftArrow))
 		{
-			GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x - 0.05f, GetComponent<Transform>().position.y, 0);
+			GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x - 0.1f, GetComponent<Transform>().position.y, 0);
 		}
 		else
 		{
@@ -52,5 +58,31 @@ public class PlayerController : MonoBehaviour
 		}
 	}
 
+	void MoveByStair()
+	{
+		if (IsGetStair == true && Input.GetKeyDown (KeyCode.UpArrow))
+		{
+			GetComponent<Transform>().position = new Vector3(GetComponent<Transform>().position.x, GetComponent<Transform>().position.y + StairHeight, 0);
+		}
+	}
 
+	void OnTriggerEnter2D(Collider2D Collider)
+	{
+		if (Collider.gameObject.tag == "Stair")
+		{
+			StairHeight = Collider.gameObject.GetComponent<StairInfo> ().StairHeight;
+			StairWidth = Collider.gameObject.GetComponent<StairInfo> ().StairWidth;
+			IsGetStair = true;
+			Debug.Log (IsGetStair);
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D Collider)
+	{
+		if (Collider.gameObject.tag == "Stair")
+		{
+			IsGetStair = false;
+			Debug.Log (IsGetStair);
+		}
+	}
 }
