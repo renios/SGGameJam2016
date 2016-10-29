c28d5c9c77423b526ed1d0ead21508f5ecb9e4c6
AirHorizons <guardian1248@snu.ac.kr>
2016-10-29 21:59:57 +0900

    Modify SoundManager
    SE source code

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -9,32 +9,82 @@ public class SoundManager : MonoBehaviour {
     public AudioSource BGM;
     public AudioSource SE;
 
+    public bool StageClear { get; set; }
+
 	// Use this for initialization
 	void Awake () {
         BGM = GetComponent<AudioSource>();
         SE = GetComponent<AudioSource>();
         BGM.loop = true;
-        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(this.gameObject);
     }
 	
-	// Update is called once per frame
-	void Update () {
-        //Debug.Log(SceneManager.GetActiveScene().name);
-        if (SceneManager.GetActiveScene().name == "StartScene" || SceneManager.GetActiveScene().name == "StageScene")
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnLevelFinishedLoading;
+    }
+
+    void OnLevelFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        //Debug.Log("Level Loaded");
+        //Debug.Log(scene.name);
+        //Debug.Log(mode);
+        this.StageClear = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Debug.Log(SceneManager.GetActiveScene().name);
+         
+        if (!StageClear)
         {
-            if ((BGM.clip == null || BGM.clip != AudioList[0]) && !BGM.isPlaying)
+            if (SceneManager.GetActiveScene().name == "StartScene" || SceneManager.GetActiveScene().name == "StageScene")
             {
-                BGM.clip = AudioList[0];
-                BGM.Play();
+                if ((BGM.clip == null || BGM.clip != AudioList[0]) && !BGM.isPlaying)
+                {
+                    BGM.clip = AudioList[0];
+                    BGM.Play();
+                }
+            }
+            else if (SceneManager.GetActiveScene().name == "1-1" || SceneManager.GetActiveScene().name == "1-2")
+            {
+                if ((BGM.clip == null || BGM.clip != AudioList[1]) && !BGM.isPlaying)
+                {
+                    BGM.clip = AudioList[1];
+                    BGM.Play();
+                }
             }
         }
-        else if (SceneManager.GetActiveScene().name == "1-2" || SceneManager.GetActiveScene().name == "1-2")
+        else
         {
-            if ((BGM.clip == null || BGM.clip != AudioList[1]) && !BGM.isPlaying)
-            {
-                BGM.clip = AudioList[1];
-                BGM.Play();
-            }
+            if (BGM.isPlaying)
+                BGM.Stop();
         }
 	}
+
+    
+
+    public void PlaySE(string name)
+    {
+        int index = matchName(name);
+        if (index < 0)
+        {
+            Debug.Log("Invalid SE Call");
+            return;
+        }
+        else
+        {
+            if (index == 2) StageClear = true;
+            SE.clip = AudioList[index];
+            SE.Play();
+        }
+    }
+
+    int matchName(string name)
+    {
+        if (name == "Clear") return 2;
+        else if (name == "Rotate") return 3;
+        else if (name == "Warp") return 4;
+        else return -1;
+    }
 }
