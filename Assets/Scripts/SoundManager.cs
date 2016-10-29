4313783bdfaa84713253adfcfa8bbfd001e29d78
AirHorizons <guardian1248@snu.ac.kr>
2016-10-29 19:21:03 +0900

    Add SoundManager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SoundManager : MonoBehaviour {
+
+    public List<AudioClip> AudioList;
+
+	// Use this for initialization
+	void Awake () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
