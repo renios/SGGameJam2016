d5cf92b76ad4a0baa5d95a79ce27cf7d2da320fa
Minjung Kim <Reny.atkins@gmail.com>
2016-10-30 11:00:56 +0900

    Recover clear effect

--- Assets/Scripts/WinButtonPopUp.cs
+++ Assets/Scripts/WinButtonPopUp.cs
@@ -5,23 +5,51 @@ using UnityEngine.SceneManagement;
 public class WinButtonPopUp : MonoBehaviour
 {
 	public GameObject Complete;
+	public GameObject WinEffect;
+	public Material BrightStarMaterial;
+
+	bool IsAlreadyPlayedWinEffect;
+	GameObject[] stars;
+
 
 	private GameObject Player;
 
 	void Start()
 	{
+		stars = GameObject.FindGameObjectsWithTag("Star_bg");
+
+		IsAlreadyPlayedWinEffect = false;
+
 		Player = GameObject.Find ("Player");
 		if (Player == null)
 		{
 			GetComponent<WinButtonPopUp> ().enabled = false;
 		}
 	}
+	
+	IEnumerator PlayWinEffect()
+	{
+		WinEffect.SetActive(true);
+		yield return new WaitForSeconds(6);
+
+		foreach (var star in stars)
+		{
+			star.GetComponent<ParticleSystemRenderer>().material = BrightStarMaterial;
+		}
+
+		yield return new WaitForSeconds(2);
+
+		Complete.SetActive (true);
+
+		IsAlreadyPlayedWinEffect = false;
+	}
 
 	void Update()
 	{
 		if (Player.GetComponent<PlayerController> ().WinGame == true)
 		{
-			Complete.SetActive (true);
+			if (!IsAlreadyPlayedWinEffect)
+				StartCoroutine(PlayWinEffect());
 		}
 		else
 			Complete.SetActive (false);
